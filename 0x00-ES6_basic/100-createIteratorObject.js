e59b1cc7a572f3557744f595e56de0ea5deafe6c
alistair000x <darmesnun43@gmail.com>
2024-03-04 12:51:42 -0800

    first advanced task

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+function* createIteratorObject(report) {
+    for (const department in report) {
+        for (const employee of report[department]) {
+            yield employee;
+        }
+    }
+}
+
+export default createIteratorObject;
