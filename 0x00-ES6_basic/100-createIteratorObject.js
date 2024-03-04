b57331f3b61e2db7a2fe0f9e6b684b4015d26970
alistair000x <darmesnun43@gmail.com>
2024-03-04 12:54:53 -0800

    fixing some issue

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,8 @@
 function* createIteratorObject(report) {
-    for (const department in report) {
-        for (const employee of report[department]) {
-            yield employee;
-        }
-    }
+for (const department in report) {
+for (const employee of report[department]) {
+yield employee;
 }
-
+}
+}
 export default createIteratorObject;
