a1ac1219c2be56c5b6a7a4ccbbddf7aae02750d6
alistair000x <darmesnun43@gmail.com>
2024-03-04 12:57:20 -0800

    2nd hard task

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+let employeeNames = [];
+for (const employee of reportWithIterator) {
+employeeNames.push(employee.name);
+}
+return employeeNames.join(' | ');
+}
