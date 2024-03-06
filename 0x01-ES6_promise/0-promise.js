ab6e25d7947cdf1020af05fa08c53b6a05120396
Alistair000x <Darmensun43@gmail.com>
2024-03-06 12:28:42 -0800

    fixing some issues

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,4 @@
 // Return a Promise using the prototype given.
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve();
-		}, 100);
-	});
+  return new Promise(() => {});
 }
